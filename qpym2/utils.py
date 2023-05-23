654adbe941e8305b83daa362358e426beba134ac
Sachi Wagaarachchi <sachi@berkeley.edu>
2023-05-23 11:23:29 -0700

    impl: utils/get_ncores according to NERSC docs (perlmutter)

--- qpym2/utils.py
+++ qpym2/utils.py
@@ -17,4 +17,31 @@ class TempConfig:
 
     def __repr__(self):
         return f'TempConfig({self._kwd_names})'
-  
+
+def get_ncores(print_state=False):
+    """ Check the number of cores available on the node. Copied from NERSC doc.
+
+    Returns:
+        int: number of cores available on the node.
+    """
+    import multiprocessing as mp
+    import os
+    import psutil
+
+    nthreads = psutil.cpu_count(logical=True)
+    ncores = psutil.cpu_count(logical=False)
+    nthreads_per_core = nthreads // ncores
+    nthreads_available = len(os.sched_getaffinity(0))
+    ncores_available = nthreads_available // nthreads_per_core
+
+    assert nthreads == os.cpu_count()
+    assert nthreads == mp.cpu_count()
+
+    if print_state:
+        print(f'{nthreads=}')
+        print(f'{ncores=}')
+        print(f'{nthreads_per_core=}')
+        print(f'{nthreads_available=}')
+        print(f'{ncores_available=}')
+
+    return ncores_available  
