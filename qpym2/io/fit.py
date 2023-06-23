a12841cc741b3668a80456e9d35aec899b15753b
Sachi Wagaarachchi <sachi@berkeley.edu>
2023-06-22 22:46:16 -0700

    added data to fit io

--- qpym2/io/fit.py
+++ qpym2/io/fit.py
@@ -1,14 +1,25 @@
 import arviz as az
 import pandas as pd
 
-def write_fit(table, trace, outdir, fname='fit'):
+def write_fit(table, trace, outdir, data=None, fname='fit', group=None):
+    """
+    TODO: implementation of group name is weird...
+    """
+    gn = group+"/" if group else ""
+    
     # TODO: change to `out_fpath`
-    az.to_netcdf(trace, f'{outdir}/{fname}.nc')
+    tf = az.to_netcdf(trace, f'{outdir}/{gn}{fname}.nc')
 
     # TODO: Is it ok to store the hists in the dataframe too? just for 5 rows?
-    table.to_hdf(f'{outdir}/{fname}.h5', key='components')
+    cf = table.to_hdf(f'{outdir}/{fname}.h5', key=f'{gn}components')
 
-def read_fit(outdir=None, fname='fit', fullname=None):
+    if data is not None:
+        s = pd.Series([p for p in data])
+        s.to_hdf(f'{outdir}/{fname}.h5', key=f'{gn}data')
+
+    return cf, tf
+
+def read_fit(outdir=None, fname='fit', fullname=None, group=None):
     """ Read fit from disk (trace and the components table)
 
     Args:
@@ -23,12 +34,18 @@ def read_fit(outdir=None, fname='fit', fullname=None):
     Returns:
         tuple: (component_table, trace)
     """
+    gn = group+"/" if group else ""
     if fullname is None:
         if outdir is None:
             raise ValueError('Either outdir or fullname must be provided')
-        fullname = f'{outdir}/{fname}'
+        fullname = f'{outdir}/{gn}{fname}'
+    
     
     trace = az.from_netcdf(fullname + '.nc')
-    table = pd.read_hdf(fullname + '.h5', key='components')
+    table = pd.read_hdf(fullname + '.h5', key=f'{gn}components')
+    try:
+        data = pd.read_hdf(fullname + '.h5', key=f'{gn}data')
+    except:
+        data = None
 
-    return table, trace
+    return table, trace, data
